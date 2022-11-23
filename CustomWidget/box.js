0f4e49a58f7efa99579d48884f27377a5381bf0b
abeygo <100276917+abeygo@users.noreply.github.com>
2022-11-23 10:55:25 +0100

    Update box.js

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -10,6 +10,28 @@
         display: block;
         }
         </style>
+        <table>
+                <thead>
+                <tr>
+                    <th>name</th>
+                    <th>height</th>
+                    <th>place</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Monte Falco</td>
+                    <td>1658</td>
+                    <td>Parco Foreste Casentinesi</td>
+                </tr>
+                <tr>
+                    <td>Monte Falterona</td>
+                    <td>1654</td>
+                    <td>Parco Foreste Casentinesi</td>
+                </tr>
+                </tbody>
+            </table>
+		
     `;
     class Box extends HTMLElement {
         constructor() {
