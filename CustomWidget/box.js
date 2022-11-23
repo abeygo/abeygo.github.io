4f13d28b82dacb9093f147b01c8228d18096f576
abeygo <100276917+abeygo@users.noreply.github.com>
2022-11-23 21:47:05 +0100

    Update box.js

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -3,66 +3,13 @@
     template.innerHTML = `
 <div>
 <table id="tabla">
-    <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Berglunds snabbköp</td>
-    <td>Christina Berglund</td>
-    <td>Sweden</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Königlich Essen</td>
-    <td>Philip Cramer</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-  <tr>
-    <td>North/South</td>
-    <td>Simon Crowther</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Paris spécialités</td>
-    <td>Marie Bertrand</td>
-    <td>France</td>
-  </tr>
+   
 </table>
 </div>
 <style>
 table, td, tr {
   border: 1px solid black;
+  background-color:blue;
 }
 </style>
 		
@@ -93,11 +40,9 @@ table, td, tr {
 	    let tabla = this._shadowRoot.getElementById("tabla").getElementsByTagName("tbody")[0];
 	    tabla.insertRow();
 	}
-	otroHeader (){
-		let tr = this._shadowRoot.getElementById('tabla').tHead;
-    		let th = document.createElement('th');
-		th.innerHTML = "Second";
-		tr.appendChild(th);
+	generateTableHead(tabla) {
+  	    let thead = table.createTHead();
+            let row = thead.insertRow();
 	}
     }
     customElements.define("com-sample-box", Box);
