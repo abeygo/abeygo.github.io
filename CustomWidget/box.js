3fa9deea6ffd558fe2ff1bdabe3c9c118a0fb959
abeygo <100276917+abeygo@users.noreply.github.com>
2022-11-28 16:50:06 +0100

    otro intento

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -1,9 +1,15 @@
-(function() {
+(function () {
     let template = document.createElement("template");
     template.innerHTML = `
 <div>
 <table id="tabla">
-   
+    <tr>
+        <th>Texte</th>
+        <th>Date de publication</th>
+        <th>Publié par</th>
+        <th>Date de publication</th>
+   </tr>
+
 </table>
 </div>
 <style>
@@ -17,7 +23,7 @@ table, td, tr {
     class Box extends HTMLElement {
         constructor() {
             super();
-            this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot = this.attachShadow({ mode: "open" });
             this._shadowRoot.appendChild(template.content.cloneNode(true));
             this.addEventListener("click", event => {
                 var event = new Event("onClick");
@@ -36,17 +42,45 @@ table, td, tr {
                 this.style["opacity"] = changedProperties["opacity"];
             }
         }
-	otraLinea (){
-	    let tabla = this._shadowRoot.getElementById("tabla");
-	    tabla.insertRow();
-	    tabla.insertRow();
-	    tabla.insertRow();
+        otraLinea() {
+            let tabla = this._shadowRoot.getElementById("tabla");
+            row=tabla.insertRow();
+                for (let i=0;i<6;i++) {
+                    row.insertCell(i)
+                }
+            
+
+        }
+        generateTableHead(table_id, separator = ',') {
+                // Select rows from table_id
+                var rows = document.querySelectorAll('table#' + table_id + ' tr');
+                // Construct csv
+                var csv = [];
+                for (var i = 0; i < rows.length; i++) {
+                    var row = [], cols = rows[i].querySelectorAll('td, th');
+                    for (var j = 0; j < cols.length; j++) {
+                        // Clean innertext to remove multiple spaces and jumpline (break csv)
+                        var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+                        // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+                        data = data.replace(/"/g, '""');
+                        // Push escaped string
+                        row.push('"' + data + '"');
+                    }
+                    csv.push(row.join(separator));
+                }
+                var csv_string = csv.join('\n');
+                // Download it
+                var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+                var link = document.createElement('a');
+                link.style.display = 'none';
+                link.setAttribute('target', '_blank');
+                link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
         
-	}
-	generateTableHead(tabla) {
-  	    let thead = table.createTHead();
-        let row = thead.insertRow();
-	}
-    }
+        }
     customElements.define("com-sample-box", Box);
 })();
