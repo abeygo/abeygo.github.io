4362f37c5a67d797ced078996eae72a09f9d65cd
abeygo <100276917+abeygo@users.noreply.github.com>
2022-11-29 11:37:57 +0100

    nuevo

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -2,13 +2,15 @@
     let template = document.createElement("template");
     template.innerHTML = `
 <div>
-<table id="tabla">
-    <tr>
-        <th>Texte</th>
-        <th>Date de publication</th>
-        <th>Publié par</th>
-        <th>Date de publication</th>
-   </tr>
+<table>
+    <tbody id="tabla">
+        <tr>
+            <th>Texte</th>
+            <th>Date de publication</th>
+            <th>Publié par</th>
+            <th>Date de publication</th>
+        </tr>
+    </tbody>
 
 </table>
 </div>
@@ -49,41 +51,22 @@ th, td {
                 this.style["opacity"] = changedProperties["opacity"];
             }
         }
-        otraLinea(length, text) {
-            let tabla = this._shadowRoot.getElementById("tabla");
-                let row = tabla.insertRow();
-                    row.insertCell(length)
-                    let cell = row.childNodes[i]
-                    cell.innerHTML = text
+        selectRow(index) {
+            let tableRow = this._shadowRoot.getElementById("tabla").row.index(index);
+            console.log(tableRow);
                 }
             
 
 
         
-        generateTableHead() {
-            var x = this._shadowRoot.getElementsByTagName("tr");
-            var txt = "";
-            var i;
-            for (i = 0; i < x.length; i++) {
-                txt = txt + "The index of Row " + (i + 1) + " is: " + x[i].rowIndex;
-                console.log(txt);
-                // Select rows from table_id
-                // Construct csv
-                /* var csv = [];
-               
-                var csv_string = csv.join('\n');
-                // Download it
-                var filename = 'export_' + new Date().toLocaleDateString() + '.csv';
-                var link = document.createElement('a');
-                link.style.display = 'none';
-                link.setAttribute('target', '_blank');
-                link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
-                link.setAttribute('download', filename);
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link); */
+        newRow() {
+            let tabla = this._shadowRoot.getElementById("tabla");
+                let row = tabla.insertRow();
             }
-        }
+        setCellText(rowIndex,cellIndex, text) {
+                this._shadowRoot.getElementById("tabla").rows[rowIndex].cells[cellIndex].innerHTML=text;
+                    
+                }
 
     }
     customElements.define("com-sample-box", Box);
---
+++ CustomWidget/buts_de_code.js
@@ -0,0 +1,21 @@
+/* var x = this._shadowRoot.getElementsByTagName("tr");
+            var txt = "";
+            var i;
+            for (i = 0; i < x.length; i++) {
+                txt = txt + "The index of Row " + (i + 1) + " is: " + x[i].rowIndex;
+                console.log(txt); */
+                // Select rows from table_id
+                // Construct csv
+                /* var csv = [];
+               
+                var csv_string = csv.join('\n');
+                // Download it
+                var filename = 'export_' + new Date().toLocaleDateString() + '.csv';
+                var link = document.createElement('a');
+                link.style.display = 'none';
+                link.setAttribute('target', '_blank');
+                link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link); */
