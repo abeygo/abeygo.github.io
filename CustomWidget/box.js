fb96d8042857e6c0eef15150de896b0e56ae7bd8
abeygo <100276917+abeygo@users.noreply.github.com>
2022-11-29 10:14:35 +0100

    ya lo mandé

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -49,47 +49,42 @@ th, td {
                 this.style["opacity"] = changedProperties["opacity"];
             }
         }
-        otraLinea(length,comment) {
+        otraLinea(length, comment) {
             let tabla = this._shadowRoot.getElementById("tabla");
-            for (let j = 0;j<length; j++) {
+            for (let j = 0; j < length; j++) {
                 let row = tabla.insertRow();
                 for (let i = 0; i < 6; i++) {
                     row.insertCell(i)
-                    let cell=row.childNodes[i]
-                    cell.innerHTML=comment
+                    let cell = row.childNodes[i]
+                    cell.innerHTML = comment
                 }
             }
 
 
         }
-        generateTableHead(table_id, separator = ',') {
-            // Select rows from table_id
-            var rows = document.querySelectorAll('table#' + table_id + ' tr');
-            // Construct csv
-            var csv = [];
-            for (var i = 0; i < rows.length; i++) {
-                var row = [], cols = rows[i].querySelectorAll('td, th');
-                for (var j = 0; j < cols.length; j++) {
-                    // Clean innertext to remove multiple spaces and jumpline (break csv)
-                    var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
-                    // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
-                    data = data.replace(/"/g, '""');
-                    // Push escaped string
-                    row.push('"' + data + '"');
-                }
-                csv.push(row.join(separator));
+        generateTableHead() {
+            var x = document.getElementsByTagName("tr");
+            var txt = "";
+            var i;
+            for (i = 0; i < x.length; i++) {
+                txt = txt + "The index of Row " + (i + 1) + " is: " + x[i].rowIndex;
+                console.log(txt);
+                // Select rows from table_id
+                // Construct csv
+                /* var csv = [];
+               
+                var csv_string = csv.join('\n');
+                // Download it
+                var filename = 'export_' + new Date().toLocaleDateString() + '.csv';
+                var link = document.createElement('a');
+                link.style.display = 'none';
+                link.setAttribute('target', '_blank');
+                link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link); */
             }
-            var csv_string = csv.join('\n');
-            // Download it
-            var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
-            var link = document.createElement('a');
-            link.style.display = 'none';
-            link.setAttribute('target', '_blank');
-            link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
-            link.setAttribute('download', filename);
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
         }
 
     }
