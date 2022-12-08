235fe491a26430fc5b13ef8c6911716b4610aa84
abeygo <100276917+abeygo@users.noreply.github.com>
2022-12-08 09:49:30 +0100

    siiii

--- CustomWidget/box.js
+++ CustomWidget/box.js
@@ -98,6 +98,21 @@ th, td {
             }
             csv_data = csv_data.join('\n');
             console.log(csv_data);
+            let CSVFile = new Blob([csv_data], {
+                type: "text/csv"
+            });
+ 
+            var temp_link = document.createElement('a');
+ 
+            temp_link.download = "Box.csv";
+            var url = window.URL.createObjectURL(CSVFile);
+            temp_link.href = url;
+ 
+            temp_link.style.display = "none";
+            document.body.appendChild(temp_link);
+ 
+            temp_link.click();
+            document.body.removeChild(temp_link);
         }
         downloadCSVFile() {
  
